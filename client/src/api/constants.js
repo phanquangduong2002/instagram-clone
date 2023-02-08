845fedc13e7568f177bc92b81f86d18302b8df55
phanquangduong2002 <phanquangduong2002@gmail.com>
2023-02-08 19:51:43 +0700

    config api

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,4 +1,4 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000/api"
-    : "http://localhost:8000/api";
+    : "https://instagram-clone-api-five.vercel.app/api";
